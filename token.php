389e3bb96f96b9f6e4ec2fa754e6ef080d309805
WolfVD <57068085+WolfVD@users.noreply.github.com>
2020-10-27 01:28:29 -0400

    changes

--- token.php
+++ token.php
@@ -2,8 +2,8 @@
 header('Access-Control-Allow-Origin: ' . $_SERVER['SERVER_NAME']);
 
 // Replace with your own subscription key and service region (e.g., "westus").
-$subscriptionKey = 'YourSubscriptionKey';
-$region = 'YourServiceRegion';
+$subscriptionKey = 'ac7bc2f70e4f4217869c9c2af2c2777f';
+$region = 'eastus';
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://' . $region . '.api.cognitive.microsoft.com/sts/v1.0/issueToken');
