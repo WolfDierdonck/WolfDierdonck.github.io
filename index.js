52f517caee72035c94a4fd178bea4ecf5e53bf2b
Wolf Van Dierdonck <57068085+WolfDierdonck@users.noreply.github.com>
2021-01-08 12:49:20 -0500

    WSMAC project

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ AOS.init({duration: 1000});
 
 var splashScreen = document.getElementById("splashScreen");
 var aboutOffset = document.getElementById("aboutOffset");
+var arGrapherImg = document.getElementById("arGrapherImg");
+var arGrapherDesc = document.getElementById("arGrapherDesc");
 
 var bottom = $(splashScreen).position().top+$(splashScreen).outerHeight(false);
 $(aboutOffset).css("height", window.innerHeight-bottom);
-
